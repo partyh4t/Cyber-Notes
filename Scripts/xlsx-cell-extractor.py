69a500f94f012f8311f33db30f751c6e72a98252
Zayd <114421293+partyh4t@users.noreply.github.com>
2024-01-05 20:43:26 -0500

    Create xlsx-cell-extractor.py

---
+++ Scripts/xlsx-cell-extractor.py
@@ -0,0 +1,15 @@
+# If we ever have a worksheet, and we want to extract all cells and print them to our terminal/save them to a file:
+
+#!/usr/bin/python3
+
+import openpyxl
+
+# Open the Excel file
+workbook = openpyxl.load_workbook(str(__import__("sys").argv[1]))
+
+# Iterate over all sheets
+for sheet in workbook.worksheets:
+    # Iterate over all cells in the sheet
+    for row in sheet.iter_rows(values_only=True):
+        print(row)
+
